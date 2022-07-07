08fdd9dafb83a589fc00ecd377a58c68182ccd8a
Viraj Patva <19it117@charusat.edu.in>
2022-07-07 10:27:15 +0530

    Add files via upload

---
+++ Token.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract SSTestToken is ERC20 {
+    bool private supplycontroled = false;
+    constructor() ERC20("SSTest Token", "SSTest") {
+         _mint(msg.sender,100000000*10 ** decimals());
+         supplycontroled = true;
+    }
+
+    function mint() external payable{
+        require(!supplycontroled, "You can't mint anymore");
+        _mint(msg.sender , msg.value);
+    } 
+}
