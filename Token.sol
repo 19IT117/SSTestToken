7ee72b2f1eaf648a585caea4381534107b52c564
Viraj Patva <19it117@charusat.edu.in>
2022-07-07 13:07:38 +0530

    Update Token.sol

--- Token.sol
+++ Token.sol
@@ -4,14 +4,16 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract SSTestToken is ERC20 {
-  //  bool private supplycontroled = false;
+    bool private supplycontroled = false;
     constructor() ERC20("SSTest Token", "SSTest") {
          _mint(msg.sender,100000000*10 ** decimals());
       //   supplycontroled = true;
     }
-
+    function decimals() public pure override returns (uint8) {
+        return 0;
+    }
     function mint() virtual external payable{
-    //    require(!supplycontroled, "You can't mint anymore");
+        require(!supplycontroled, "You can't mint anymore");
         _mint(msg.sender , msg.value);
     } 
 }
